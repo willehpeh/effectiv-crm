e728ea377d7929c9c17a8bc261afa8f2351a0dd5
w.alexander <will_alexander@me.com>
2025-07-18 22:18:21 +0200

    Provide UI facade

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -5,11 +5,17 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { UiFacade } from './ui/ui.facade';
+import { InMemoryUiFacade } from './ui/in-memory.ui.facade';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    {
+      provide: UiFacade,
+      useClass: InMemoryUiFacade,
+    }
   ],
 };

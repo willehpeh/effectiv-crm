93241a00bf751652f8a65022fcbf85749b299dbe
w.alexander <will_alexander@me.com>
2025-07-24 23:03:34 +0200

    Add files to start lead capture dev

---
+++ packages/application/src/leads/commands/capture-lead/index.ts
@@ -0,0 +1,5 @@
+export * from './capture-lead.dto';
+export * from './capture-lead.command';
+export * from './capture-lead.handler';
+
+
--- packages/tests/src/leads/commands/capture-lead/capture-lead.spec.ts
+++ packages/tests/src/leads/commands/capture-lead/capture-lead.spec.ts
@@ -1,13 +1,13 @@
-import { EventStore } from '@effectiv-crm/domain';
-
-class CaptureLeadCommand {
-}
-
-class CaptureLeadCommandHandler {
-}
+import { FakeEventStore } from '../../../test-doubles/fake.event-store';
+import { CaptureLeadCommand, CaptureLeadCommandHandler, CaptureLeadDto } from '@effectiv-crm/application';
 
 describe('Capture Lead', () => {
+  let dto: CaptureLeadDto;
   let command: CaptureLeadCommand;
   let handler: CaptureLeadCommandHandler;
-  let eventStore: EventStore;
+  let eventStore: FakeEventStore;
+
+  it('placeholder', () => {
+    expect(true).toBe(true);
+  });
 });

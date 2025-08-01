20b7b34dca2c2e32564ec3aa9453b47fdd441006
w.alexander <will_alexander@me.com>
2025-08-01 22:06:53 +0200

    Lint fix

--- packages/infrastructure/src/database/database-module.factory.ts
+++ packages/infrastructure/src/database/database-module.factory.ts
@@ -2,7 +2,7 @@ import { DatabaseModule } from './database.module';
 import { SqliteDatabaseModule } from './sqlite-database.module';
 
 export function getDatabaseModule() {
-  return process.env.DB_TYPE === 'sqlite' 
-    ? SqliteDatabaseModule 
+  return process.env['DB_TYPE'] === 'sqlite'
+    ? SqliteDatabaseModule
     : DatabaseModule;
 }

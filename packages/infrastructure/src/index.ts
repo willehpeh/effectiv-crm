745ad8cd052fa6f4766e8003b36cedd06c5c3ab0
w.alexander <will_alexander@me.com>
2025-08-06 22:44:57 +0200

    Rename projection files

--- packages/infrastructure/src/index.ts
+++ packages/infrastructure/src/index.ts
@@ -7,6 +7,6 @@ export { MikroOrmEventStore } from './database/mikro-orm-event-store';
 export { EventEntity } from './database/entities/event.entity';
 export { AuthContext } from './context/auth.context';
 export { RequestContext } from './context/request.context';
-export { InMemoryContactProjection } from './projections/in-memory-contact-projection';
-export { InMemoryLeadsProjection } from './projections/in-memory-leads-projection';
+export { InMemoryContactProjection } from './projections/in-memory.contact-projection';
+export { InMemoryLeadsProjection } from './projections/in-memory.leads-projection';
 export { NestjsEventPublisher } from './events/nestjs-event-publisher';
